2b12a109073cc164fbd385a790146370a733d0e2
mamg22 <45301823+mamg22@users.noreply.github.com>
2019-09-19 21:29:42 -0400

    apply scrollback-mouse patch

--- st.h
+++ st.h
@@ -77,6 +77,13 @@ typedef union {
 	const void *v;
 } Arg;
 
+typedef struct {
+	 uint b;
+	 uint mask;
+	 void (*func)(const Arg *);
+	 const Arg arg;
+} MouseKey;
+
 void die(const char *, ...);
 void redraw(void);
 void draw(void);
@@ -131,4 +138,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern MouseKey mkeys[];
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
