14d0d3274d47aa67b1885b43dc67abd7c5a4e6fb
mamg22 <45301823+mamg22@users.noreply.github.com>
2020-05-16 21:22:36 -0400

    Apply alpha patch

--- st.h
+++ st.h
@@ -133,4 +133,5 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
+extern float alpha;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
